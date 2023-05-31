108ca5e3ac563ddd5278f511bc1e9a45f0d79378
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-05-31 16:37:38 +0200

    [finale] add final function of extended duration parser

---
+++ index.ts
@@ -0,0 +1,43 @@
+import ms from "ms";
+import parseStringDuration from "parse-duration";
+
+const colonRegex = new RegExp(/(0?\d|1[0-9]|2[0-3]):([0-5]?\d)(:([0-5]?\d)(:([0-5]?\d))?)?/);
+const digitsOnly = new RegExp(/^\d{1,}$/);
+
+export default function(val: string) {
+  if (val.match(colonRegex)) {
+    const colony = val.split(":").map(val => parseInt(val));
+
+    switch (colony.length) {
+      case 4: {
+        const [days, hours, minutes, seconds] = colony;
+        return (days * ms("1d")) + (hours * ms("1h")) + (minutes * ms("1m")) + (seconds * ms("1s"));
+      };
+
+      case 3: {
+        const [hours, minutes, seconds] = colony;
+        return (hours * ms("1h")) + (minutes * ms("1m")) + (seconds * ms("1s"));
+      }
+
+      case 2: {
+        const [minutes, seconds] = colony;
+        return (minutes * ms("1m")) + (seconds * ms("1s"));
+      };
+
+      default: return NaN;
+    };
+  };
+
+  let time = ms(digitsOnly.test(val) ? val + "s" : val);
+
+  if (!isNaN(time)) {
+    return time;
+  } else {
+    time = parseStringDuration(val);
+    if (!isNaN(time)) {
+      return time;
+    } else {
+      return NaN;
+    };
+  };
+};
